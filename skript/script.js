062496f6cb1a165f173c4c939bc98000c7cba156
Карина <karen.demy.dark@gmail.com>
2022-09-21 14:08:54 +0300

    created project

---
+++ skript/script.js
@@ -0,0 +1,13 @@
+const colorEyes = '#800000';
+let user ;
+let userName = 'Karina';
+user = userName;
+console.log (user);
+let myColorEyes ;
+myColorEyes = colorEyes;
+console.log (myColorEyes);
+console.log ('20' + '22');
+console.log (20 +22);
+console.log (typeof colorEyes);
+let age;
+console.log (typeof age);
