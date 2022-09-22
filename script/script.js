145e003a57a11ddbbc9fc380b96aea904a64913d
Карина <karen.demy.dark@gmail.com>
2022-09-22 15:34:51 +0300

    change js

---
+++ script/script.js
@@ -0,0 +1,16 @@
+alert ('Я – JavaScript!');
+
+let name = 'Ivan';
+let admin ;
+admin = name ;
+alert (admin);
+
+let nameForPlanet;
+let curentUserName;
+
+alert (`привіт ${1}`);
+alert (`привіт ${"name"}`); 
+alert (`привіт ${name}`); 
+
+curentUserName = prompt('Як тебе звуть?');
+alert (`Привіт, ${curentUserName}!Ми раді знайомству)`);
--- skript/script.js
+++ skript/script.js
@@ -1,16 +1,20 @@
-alert ('Я – JavaScript!');
+//alert ('Я – JavaScript!');
 
-let name = 'Ivan';
-let admin ;
-admin = name ;
-alert (admin);
+//let name = 'Ivan';
+//let admin ;
+//admin = name ;
+//alert (admin);
 
-let nameForPlanet;
-let curentUserName;
+//let nameForPlanet;
+//let curentUserName;
 
-alert (`привіт ${1}`);
-alert (`привіт ${"name"}`); 
-alert (`привіт ${name}`); 
+//alert (`привіт ${1}`);
+//alert (`привіт ${"name"}`); 
+//alert (`привіт ${name}`); 
 
-curentUserName = prompt('Як тебе звуть?');
-alert (`Привіт, ${curentUserName}!Ми раді знайомству)`);
+//curentUserName = prompt('Як тебе звуть?');
+//alert (`Привіт, ${curentUserName}!Ми раді знайомству)`); 
+
+let hi;
+hi = "Привет";
+alert (+hi);
